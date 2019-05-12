697cb72bbef75abcb86094a71f2d47ffaa2d2b8a
Miriam Mendes <39604184+minnmi@users.noreply.github.com>
2019-05-12 15:59:33 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IFMT - Curso Básico de Bootstrap</title>
+	<link rel="shortcut icon" href="https://fabiosmariano.herokuapp.com/favicon.png" />
+	<!-- OPEN GRAPH FACEBOOK -->
+  <meta property="og:locale" content="pt_BR">
+  <meta property="og:title" content="IFMT - Curso Básico de Bootstrap"/>
+  <meta property= "og:image" content="https://fabiosmariano.herokuapp.com/cursobootstrap/divulgacao.jpg"/>
+  <meta property="og:description" content="Curso Básico de Bootstrap v4.0 - Ministrado no IFMT Campus Octayde Jorge em Cuiabá/MT">
+  <meta property="og:url" content="https://fabiosmariano.herokuapp.com/cursobootstrap">
+  <meta property="og:type" content="website">
+  <!--
+  <meta name="fb:page_id" content="276019319736697" />
+-->
+  
+  <meta name="og:email" content="fabio.smariano@gmail.com"/>
+
+  <meta name="og:locality" content="Cuiabá"/>
+  <meta name="og:region" content="MT"/>
+  <meta name="og:country-name" content="BR"/>
+
+ 
+  <!-- TAGS SEO -->
+	<meta name="keywords" content="IFMT, UAB, BOOTSTRAP, MINI CURSO"/>
+	<meta name="description" content="Curso Básico de Bootstrap v4.0 - Ministrado no IFMT Campus Octayde Jorge em Cuiabá/MT"/>
+	<meta name="copyright"content="Fabio Mariano">
+	<meta name="content-language" content="pt-br">
+	<meta name="robots" content="index,follow" />
+	<meta name="revised" content="Quinta-feira, 07 de março, 2019, 06:00 pm" />
+	<meta name="author" content="Fabio Mariano">
+	<meta name="designer" content="Fabio Mariano">
+	<meta name="copyright" content="@2019 Fabio Mariano">
+	<meta name="owner" content="Fabio Mariano">
+	<meta name="url" content="https://fabiosmariano.herokuapp.com/cursobootstrap">
+	<meta name="identifier-URL" content="https://fabiosmariano.herokuapp.com/cursobootstrap">
+	<meta name="rating" content="General">
+	<meta name="revisit-after" content="7 days">
+	<meta http-equiv="Expires" content="0">
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="Cache-Control" content="no-cache">
+	<meta http-equiv="imagetoolbar" content="no" />
+
+</head>
+<body>
+<center class="centralizar">
+	<a href="http://fabiosmariano.herokuapp.com/cursobootstrap/aula.html">
+		<img src="divulgacao.jpg">
+	</a>
+</center>
+</body>
+</html>
